3de87fdaf18195ed3273e2df37081416028b5d49
Daniel Parpal <daniel.parpal@shopify.com>
2019-11-06 16:50:41 -0500

    Added the turn switching (base) system

--- src/Game.tsx
+++ src/Game.tsx
@@ -2,14 +2,33 @@ import React, {useState} from 'react';
 import './Game.css';
 
 const Game: React.FC = () => {
-  const [grid, setGrid] = useState([[0, 0, 0], [0, 0, 0], [0, 0, 0]]);
+  const TurnsEnum = {
+    X: 'X',
+    O: 'O'
+  };
+
+  const [grid, setGrid] = useState([['', '', ''], ['', '', ''], ['', '', '']]);
+  const [turn, setTurn] = useState(TurnsEnum.X);
+
+  const toggleTile = (row: number, col: number, e: React.SyntheticEvent) => {
+    console.log("row", row);
+    console.log("col", col);
+    const currentState = grid[row][col];
+    if (currentState !== '') return;
+    
+    const newGrid = grid;
+    newGrid[row][col] = turn;
+    setGrid([...grid]);
+    const newTurn = turn === TurnsEnum.X ? TurnsEnum.O : TurnsEnum.X;
+    setTurn(newTurn);
+  }
   
-  const tiles = grid.map((row, indexRow) => {
+  const tiles = grid.map((row, rowIndex) => {
     return (
-      <div key={'row-' + indexRow}>
-        {row.map((tile, indexCol) => {
+      <div key={'row-' + rowIndex}>
+        {row.map((tile, colIndex) => {
           return (
-            <div className='Tile' key={'col-' + indexRow + '-' + indexCol}>
+            <div onClick={(e) => toggleTile(rowIndex, colIndex, e)} className='Tile' key={'col-' + rowIndex + '-' + colIndex}>
               {tile}
             </div>
           );
@@ -17,8 +36,6 @@ const Game: React.FC = () => {
       </div>
     );
   });
-
-  console.log(tiles);
 
   return (
     <div>{tiles}</div>
