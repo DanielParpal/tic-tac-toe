cbc33bde13df1c218d10413002a2458e6d2e4d2b
Daniel Parpal <daniel.parpal@shopify.com>
2019-11-06 15:59:06 -0500

    Created the base Game component

---
+++ src/Game.tsx
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+const Game: React.FC = () => {
+  const [tiles, setTiles] = useState([[0, 0, 0], [0, 0, 0], [0, 0, 0]]);
+
+  return <p>Hello</p>;
+}
+
+export default Game;
