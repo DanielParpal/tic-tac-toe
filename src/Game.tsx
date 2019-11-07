83acd57def4cf4c4ee6da76bf4057a85e5a0491b
Daniel Parpal <daniel.parpal@shopify.com>
2019-11-07 09:37:07 -0500

    Adjusted the Game component to use the new engine

--- src/Game.tsx
+++ src/Game.tsx
@@ -4,8 +4,8 @@ import {checkForWin} from './modules/engine';
 
 const Game: React.FC = () => {
   const TurnsEnum = {
-    X: 'X',
-    O: 'O'
+    X: 'x',
+    O: 'o'
   };
 
   const [grid, setGrid] = useState([['', '', ''], ['', '', ''], ['', '', '']]);
@@ -19,122 +19,14 @@ const Game: React.FC = () => {
     const newGrid = grid;
     newGrid[row][col] = turn;
     setGrid([...grid]);
-    console.log(grid);
 
-    checkForWinner();
+    const gameWon = checkForWin(grid.flat(), turn);
 
-    if (winner === '') {
+    if (gameWon) {
+      setWinner('x')
+    } else {
       const newTurn = turn === TurnsEnum.X ? TurnsEnum.O : TurnsEnum.X;
       setTurn(newTurn);
-    }
-  }
-
-  const checkForWinner = (): string => {
-    checkRows();
-    checkColumns();
-    checkDiagonal1();
-    checkDiagonal2();
-    
-    return "ABC";
-  };
-
-  const checkRows = () => {
-    for (const i of [0, 1, 2]) {
-      let winner = '';
-      for (const j of [0, 1, 2]) {
-        const tile = grid[i][j];
-        if (tile === '') break;
-        if (j === 0) {
-          winner = tile;
-          continue;
-        } else {
-          if (tile === winner) {
-            if (j === 2) {
-              console.log("rows", i);
-              setWinner(tile);
-              return;
-            } else {
-              continue;
-            }
-          } else {
-            break;
-          }
-        }
-      }
-    }
-  }
-  
-  const checkColumns = () => {
-    for (const i of [0, 1, 2]) {
-      let winner = '';
-      for (const j of [0, 1, 2]) {
-        const tile = grid[j][i];
-        if (tile === '') break;
-        if (j === 0) {
-          winner = tile;
-          continue;
-        } else {
-          if (tile === winner) {
-            if (j === 2) {
-              console.log("columns");
-              setWinner(tile);
-              return;
-            } else {
-              continue;
-            }
-          } else {
-            break;
-          }
-        }
-      }
-    }
-  }
-
-  const checkDiagonal1 = () => {
-    let winner = '';
-    for (const i of [0, 1, 2]) {
-      const tile = grid[i][i];
-      if (tile === '') break;
-      if (i === 0) {
-        winner = tile;
-        continue;
-      } else {
-        if (tile === winner) {
-          if (i === 2) {
-            console.log("diag 1");
-            setWinner(tile);
-            return;
-          } else {
-            continue;
-          }
-        } else {
-          break;
-        }
-      } 
-    }
-  }
-
-  const checkDiagonal2 = () => {
-    let winner = '';
-    for (const i of [0, 1, 2]) {
-      const tile = grid[i][2-i];
-      if (tile === '') break;
-      if (i === 0) {
-        winner = tile;
-        continue;
-      } else {
-        if (tile === winner) {
-          if (i === 2) {
-            console.log("diag 2");
-            setWinner(tile);
-            return;
-          } else {
-            continue;
-          }
-        } else {
-          break;
-        }
-      } 
     }
   }
 
